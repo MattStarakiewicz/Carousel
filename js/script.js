13c0626a566a35e990ff296052b5580ccf876d44
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-02-07 01:54:49 +0100

    add slides

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+'use strict';
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  pageDots: false,
+  hash: true
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var buttonGroup = document.querySelector('.button-reset');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
 });
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
