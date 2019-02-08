682ab74026d57583889404ab50ae53b4a7655495
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-02-09 00:10:56 +0100

    add google map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//HTML TEMPLATE
+
 var slideItem = document.getElementById('slide-item').innerHTML;
 
 Mustache.parse(slideItem);
@@ -13,6 +15,8 @@ for(var i = 0; i < slidesData.length; i++){
 
 var results = document.querySelector('#results');
 results.insertAdjacentHTML('beforeend', listItem);
+
+// CAROUSEL
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -36,10 +40,31 @@ buttonGroup.addEventListener( 'click', function( event ) {
   flkty.select( index );
 });
 
+// CAROUSEL PROGRESSBAR
+
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// MAP
+
+window.initMap = function(){
+    var coords1 = {lat: 51.24882129, lng: 22.56860822};   
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 19, center: coords1});
+    // The marker, positioned at Uluru
+
+    for(var i = 0; i < slidesData.length; i++){
+      console.log(slidesData);
+      var marker = new google.maps.Marker({position: slidesData[i].coords, map: map});
+    }
+    
+
+   // var marker = new google.maps.Marker({position: coords1, map: map});
+}
+
 
